46315b3950402e81524e37469cae5f0ef5d2f99f
Ming Zhang <ming.zhang@nxp.com>
2024-10-20 07:20:04 +0800

    UPDATE CODE

--- C语言程序设计与项目案例教程（微课视频版）/chap_04/exp_04_09.c
+++ C语言程序设计与项目案例教程（微课视频版）/chap_04/exp_04_09.c
@@ -1,7 +1,26 @@
 /*
 *   
 * 
-*   样例输出：
+*   样例：
+*       请输入密码：1234
+*       请输入一个整数(1~100):30
+*       您输入的数是 30.数值太小了，按任意键重试！
+*       请输入一个整数(1~100):40
+*       您输入的数是 40.数值小了些，按任意键重试！
+*       请输入一个整数(1~100):50
+*       您输入的数是 50.数值小了些，按任意键重试！
+*       请输入一个整数(1~100):70
+*       您输入的数是 70.数值大了些，按任意键重试！
+*       请输入一个整数(1~100):60
+*       您输入的数是 60.对不起，只大了一点！按任意键重试！
+*       请输入一个整数(1~100):55
+*       您输入的数是 55.对不起，只小了一点！按任意键重试！
+*       请输入一个整数(1~100):56
+*       您输入的数是 56.对不起，只小了一点！按任意键重试！
+*       请输入一个整数(1~100):57
+*       您输入的数是 57.对不起，只小了一点！按任意键重试！
+*       请输入一个整数(1~100):58
+*       您输入的数是 58.太好了！您猜对了！再见！
 */
 
 #include <stdio.h>
@@ -12,9 +31,9 @@ int main(void)
     int password = 0;
     int number = 0;
     int price = 58;
-    while(password != 1234) 
+    while (password != 1234)
     {
-        if(i > 3)
+        if (i > 3)
         {
             return 0;
         }
@@ -24,15 +43,15 @@ int main(void)
     }
     i = 0;
 
-    while(number != price)
+    while (number != price)
     {
         do {
             printf("请输入一个整数(1~100):");
             scanf("%d", &number);
             printf("您输入的数是 %d.", number);
-        } while(!(number >= 1 && number <= 100));
+        } while (!(number >= 1 && number <= 100));
 
-        if(number >= 90)
+        if (number >= 90)
         {
             printf("数值太大了，按任意键重试！\n");
         }
@@ -40,7 +59,7 @@ int main(void)
         {
             printf("数值大了些，按任意键重试！\n");
         }
-        else if (number >= 1 && number <= 30) 
+        else if (number >= 1 && number <= 30)
         {
             printf("数值太小了，按任意键重试！\n");
         }
@@ -50,7 +69,7 @@ int main(void)
         }
         else
         {
-            if(number == price)
+            if (number == price)
             {
                 printf("太好了！您猜对了！再见！\n");
             }
@@ -63,7 +82,7 @@ int main(void)
                 printf("对不起，只大了一点！按任意键重试！\n");
             }
         }
+        getchar();
     }
-    getchar();
     return 0;
 }
