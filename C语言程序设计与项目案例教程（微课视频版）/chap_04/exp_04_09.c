7158617aaed93a8a8185f0b49b204ec8a065a284
Ming Zhang <ming.zhang@nxp.com>
2024-10-20 01:28:21 +0800

    upload code

--- C语言程序设计与项目案例教程（微课视频版）/chap_04/exp_04_09.c
+++ C语言程序设计与项目案例教程（微课视频版）/chap_04/exp_04_09.c
@@ -8,6 +8,62 @@
 
 int main(void)
 {
+    int i = 0;
+    int password = 0;
+    int number = 0;
+    int price = 58;
+    while(password != 1234) 
+    {
+        if(i > 3)
+        {
+            return 0;
+        }
+        i++;
+        printf("请输入密码：");
+        scanf("%d", &password);
+    }
+    i = 0;
 
+    while(number != price)
+    {
+        do {
+            printf("请输入一个整数(1~100):");
+            scanf("%d", &number);
+            printf("您输入的数是 %d.", number);
+        } while(!(number >= 1 && number <= 100));
+
+        if(number >= 90)
+        {
+            printf("数值太大了，按任意键重试！\n");
+        }
+        else if (number >= 70 && number < 90)
+        {
+            printf("数值大了些，按任意键重试！\n");
+        }
+        else if (number >= 1 && number <= 30) 
+        {
+            printf("数值太小了，按任意键重试！\n");
+        }
+        else if (number > 30 && number <= 50)
+        {
+            printf("数值小了些，按任意键重试！\n");
+        }
+        else
+        {
+            if(number == price)
+            {
+                printf("太好了！您猜对了！再见！\n");
+            }
+            else if (number < price)
+            {
+                printf("对不起，只小了一点！按任意键重试！\n");
+            }
+            else if (number > price)
+            {
+                printf("对不起，只大了一点！按任意键重试！\n");
+            }
+        }
+    }
+    getchar();
     return 0;
 }
