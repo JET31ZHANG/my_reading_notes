71286d07ad3845b8017f852c391f779d1d9799f7
Ming Zhang <ming.zhang@nxp.com>
2024-10-29 07:29:42 +0800

    Update exp_07_10.c

--- C语言程序设计与项目案例教程（微课视频版）/chap_07/exp_07_10.c
+++ C语言程序设计与项目案例教程（微课视频版）/chap_07/exp_07_10.c
@@ -7,6 +7,13 @@
 
 int main(void)
 {
+    /*
+    *   其含义相当于：
+    *               char a[] = "I Love China!"
+    *               * s;
+    *               s = a;
+    *   数组a 是由 C 语言环境隐含给出的
+    */
     char *s = "I Love China!";
     printf("%s\n", s);
 
