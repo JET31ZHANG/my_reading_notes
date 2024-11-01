03f7521515144610c9e2c20997bf3b62e664dee9
JET31ZHANG <735209541@qq.com>
2024-11-01 22:35:03 +0800

    Update practice_08.c

--- C语言程序设计与项目案例教程（微课视频版）/chap_08/practice_08.c
+++ C语言程序设计与项目案例教程（微课视频版）/chap_08/practice_08.c
@@ -9,11 +9,18 @@
 *
 *   实战内容：编程实现输入 “一带一路” 中线城市编号、城市名称、城市面积和城市简介，并在屏幕上显示该信息
 *
-*   样例输入：
+*   样例输入：请输入中线城市个数：2
+*            请输入中线城市编号、城市名称、城市面积（平方千米）、城市简介
+*            1 北京 16410.54 是中华人民共和国首都，政治、经济、文化中心
+*            请输入中线城市编号、城市名称、城市面积（平方千米）、城市简介
+*            2 西安 10108 是西部中心城市
 *
-*   样例输出：
+*   样例输出：   城市编号        城市名称        城市面积        城市简介
+*               1               北京            16410.54        是中华人民共和国首都，政治、经济、文化中心
+*               2               西安            10108.00        是西部中心城市
 */
 #include <stdio.h>
+#include <malloc.h>
 
 struct introduction
 {
@@ -24,8 +31,51 @@ struct introduction
     struct introduction *next;
 };
 
+struct introduction *create(int n)
+{
+    struct introduction *head, *pf, *pb;
+    int i;
+    for(i = 0; i < n; i++)
+    {
+        pb = (struct introduction *) malloc (sizeof(struct introduction));
+        printf("请输入中线城市编号、城市名称、城市面积（平方千米）、城市简介\n");
+        scanf("%d%s%f%s", &pb -> num, &pb -> name, &pb -> area, &pb -> city);
+
+        if(i == 0)
+        {
+            pf = head = pb;
+        }
+        else 
+        {
+            pf -> next = pb;
+        }
+
+        pb -> next = NULL;
+        pf = pb;
+    }
+    return (head);
+}
+
+void print(struct introduction *head)
+{
+    printf("城市编号\t城市名称\t城市面积\t城市简介\n");
+    while(head != NULL)
+    {
+        printf("%d\t\t%s\t\t%4.2f\t%s\n", head -> num, head -> name, head -> area, head -> city);
+        head = head -> next;
+    }
+}
+
 int main(void)
 {
+    struct introduction *head;
+    int n;
+
+    printf("请输入中线城市个数：");
+    scanf("%d", &n);
+
+    head = create(n);
+    print(head);
 
     return 0;
 }
