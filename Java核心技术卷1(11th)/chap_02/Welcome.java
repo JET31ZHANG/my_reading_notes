1afd49543d8a89f150ba7aecb3cbb8963a0b4ce6
JET31ZHANG <735209541@qq.com>
2024-11-10 20:53:07 +0800

    upload

---
+++ Java核心技术卷1(11th)/chap_02/Welcome.java
@@ -0,0 +1,42 @@
+/**
+ * This program displays a greeting from the author.
+ * @version 1.30 2014-02-27
+ * @author Cay Horstmann
+*/
+
+/*
+ * Ouput: 
+Welcome to Core Java
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+=
+*/
+
+public class Welcome
+{
+    public static void main(String[] args)
+    {
+        String greeting = "Welcome to Core Java";
+        System.out.println(greeting);
+        for(int i = 0; i < greeting.length(); i++)
+            System.out.println("=");
+        System.out.println();
+    }
+}
